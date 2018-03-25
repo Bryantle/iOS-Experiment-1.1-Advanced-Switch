457305fc53ac3b9ee958a19fe46ddf1389af6052
Bryantle <bryantl@berkeley.edu>
2018-03-25 14:48:04 -0700

    Fixed tint color change.

--- iOS Experiment 1.1 Advanced Switch/ViewController.swift
+++ iOS Experiment 1.1 Advanced Switch/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonOutlet: UISwitch!
+    @IBAction func buttonAction(_ sender: Any) {
+        if buttonOutlet.isOn{
+        self.view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+            buttonOutlet.onTintColor = #colorLiteral(red: 0.9169482246, green: 1, blue: 0, alpha: 1)
+            buttonOutlet.backgroundColor = self.view.backgroundColor
+        }
+        else {
+            self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            buttonOutlet.onTintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            buttonOutlet.backgroundColor = self.view.backgroundColor
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        buttonOutlet.setOn(false, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
